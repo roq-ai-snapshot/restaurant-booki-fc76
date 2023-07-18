8e3dd07d6dec8704e7b5265eb5a2771a20192ece
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-07-18 12:41:28 +0000

    Initial commit

---
+++ src/pages/reservations/view/[id]/index.tsx
@@ -0,0 +1,130 @@
+import { Box, Center, Flex, Link, List, ListItem, Spinner, Stack, Text } from '@chakra-ui/react';
+import Breadcrumbs from 'components/breadcrumb';
+import { Error } from 'components/error';
+import { FormListItem } from 'components/form-list-item';
+import { FormWrapper } from 'components/form-wrapper';
+import AppLayout from 'layout/app-layout';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import { FunctionComponent, useState } from 'react';
+import { routes } from 'routes';
+import useSWR from 'swr';
+import { compose } from 'lib/compose';
+import {
+  AccessOperationEnum,
+  AccessServiceEnum,
+  requireNextAuth,
+  useAuthorizationApi,
+  withAuthorization,
+} from '@roq/nextjs';
+import { UserPageTable } from 'components/user-page-table';
+
+import { getReservationById } from 'apiSdk/reservations';
+import { ReservationInterface } from 'interfaces/reservation';
+
+function ReservationViewPage() {
+  const { hasAccess } = useAuthorizationApi();
+  const router = useRouter();
+  const id = router.query.id as string;
+  const { data, error, isLoading, mutate } = useSWR<ReservationInterface>(
+    () => (id ? `/reservations/${id}` : null),
+    () =>
+      getReservationById(id, {
+        relations: ['user', 'table'],
+      }),
+  );
+
+  const [deleteError, setDeleteError] = useState(null);
+  const [createError, setCreateError] = useState(null);
+
+  return (
+    <AppLayout
+      breadcrumbs={
+        <Breadcrumbs
+          items={[
+            {
+              label: 'Reservations',
+              link: '/reservations',
+            },
+            {
+              label: 'Reservation Details',
+              isCurrent: true,
+            },
+          ]}
+        />
+      }
+    >
+      <Box rounded="md">
+        {error && (
+          <Box mb={4}>
+            <Error error={error} />
+          </Box>
+        )}
+        {isLoading ? (
+          <Center>
+            <Spinner />
+          </Center>
+        ) : (
+          <>
+            <FormWrapper>
+              <Stack direction="column" spacing={2} mb={4}>
+                <Text
+                  sx={{
+                    fontSize: '1.875rem',
+                    fontWeight: 700,
+                    color: 'base.content',
+                  }}
+                >
+                  Reservation Details
+                </Text>
+              </Stack>
+              <List spacing={3} w="100%">
+                <FormListItem label="Date:" text={data?.date as unknown as string} />
+
+                <FormListItem label="Time:" text={data?.time as unknown as string} />
+
+                <FormListItem label="Guest Count:" text={data?.guest_count} />
+
+                <FormListItem label="Created At:" text={data?.created_at as unknown as string} />
+
+                <FormListItem label="Updated At:" text={data?.updated_at as unknown as string} />
+
+                {hasAccess('user', AccessOperationEnum.READ, AccessServiceEnum.PROJECT) && (
+                  <FormListItem
+                    label="User:"
+                    text={
+                      <Link as={NextLink} href={`/users/view/${data?.user?.id}`}>
+                        {data?.user?.email}
+                      </Link>
+                    }
+                  />
+                )}
+                {hasAccess('table', AccessOperationEnum.READ, AccessServiceEnum.PROJECT) && (
+                  <FormListItem
+                    label="Table:"
+                    text={
+                      <Link as={NextLink} href={`/tables/view/${data?.table?.id}`}>
+                        {data?.table?.number}
+                      </Link>
+                    }
+                  />
+                )}
+              </List>
+            </FormWrapper>
+          </>
+        )}
+      </Box>
+    </AppLayout>
+  );
+}
+
+export default compose(
+  requireNextAuth({
+    redirectTo: '/',
+  }),
+  withAuthorization({
+    service: AccessServiceEnum.PROJECT,
+    entity: 'reservation',
+    operation: AccessOperationEnum.READ,
+  }),
+)(ReservationViewPage);
